f3e9a6f0de59a1efc9e0a0b4ef0ddd747f08311b
Charlie <Charlie@Charlie-DELL>
2014-04-23 20:08:25 -0500

    add a new py module

---
+++ CallPython/pyM.py
@@ -0,0 +1,16 @@
+'''
+Created on Apr 23, 2014
+
+@author: Charlie
+'''
+
+
+def add(a,b):
+    return a+b
+
+def addFixedValue(a):
+  y = 5
+  return y +a
+  
+print add(5,2)
+print addFixedValue(1) 
